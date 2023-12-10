db981f3bbca0e8a02aa63939292d4191550e3a57
HodaBarghash <121548764+HodaBarghash@users.noreply.github.com>
2023-12-10 17:25:54 +0200

    g

--- story_content/user.js
+++ story_content/user.js
@@ -2,6 +2,26 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "6DnkRvHAqK0":
+        Script1();
+        break;
   }
+}
+
+function Script1()
+{
+  const url="https://script.google.com/macros/s/AKfycbw0t7Yw13RrgBrLo3BN34FV55Vxufiy7hG_oSc67HZvWYl1EZaROZY9Y8LH7h0PwZjs/exec"
+
+var player=GetPlayer()
+
+fetch(url,{
+	method:"POST",
+	mode: 'no-cors',
+	cache:'no-cache',
+	headers: {'Content-Type': 'application/json'},
+	redirect:'follow',
+	body: JSON.stringify({v4q2attempts:player.GetVar("v4q2attempts"),v4q2answeredcorrect:player.GetVar("v4q2answeredcorrect")})
+	}
+	)
 }
 
